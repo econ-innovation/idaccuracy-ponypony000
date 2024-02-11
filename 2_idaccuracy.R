7e491642bfe4332ca460b993ca616685ea919d22
minamwt@163.com <mawenting@xiaomadeMacBook-Air-2.local>
2024-02-11 12:54:46 +0800

    2

--- 2_idaccuracy.R
+++ 2_idaccuracy.R
@@ -3,23 +3,23 @@
 #作业一
 
 #1
-
-getwd() 
+#导入在r中进行数据读取的包
+library(readr)
 
 #定义要读取的数据的路径
+getwd() 
 datapath="/Users/mawenting/Documents/Bigdata_econ/Assignment/Assignment_data/assignment_idaccuracy/Aminer"
 file.exists(datapath) 
-
-# 设定datapath为⼯作路径 
 setwd(datapath)
 
-#提取当前工作目录下的文件名并生成一个列表
+#list.files() 提取当前工作目录下的文件名并存入一个列表
 list <- list.files() 
+#list <- list.files(pattern="*.csv") #只读取csv文件
 
-#建立一个空的数据框
+#建立一个空的数据框，用于之后的数据合并
 df <- data.frame()
 
-#将每一个csv转化为数据框并纵向合并
+#在r中逐条读取列表里的内容转化为数据框，并纵向合并rbind()数据框
 for(i in list){
   
   tempdf=read.csv(file=i)
@@ -29,17 +29,23 @@ for(i in list){
 
 head(df)
 
+#写入文件
+write.csv(df,file="/Users/mawenting/Documents/Bigdata_econ/Assignment")
+
 #2使用lapply和do.call读取并合并多个文件
 
-#lapply输出的是list并未合并，使用do.call合并
+#lappy对list里的每一个文件名执行read.csv函数，读取csv，每一个文件作为单独数据框保存到data_list里
 
 data_list <- lapply(list, read.csv)
+
+#使用do.call合并，do.call调用rind()函数，并将data_list里每一个单独数据框依次传递
 df1 <- do.call(rbind, data_list)
 
 head(df1)
 
 #3在命令行运行以下命令
 
+# cd 
 # #!/usr/bin/env Rscript
 # Rscript  /Users/mawenting/Documents/Bigdata_econ/Assignment/add.R /Users/mawenting/Documents/Bigdata_econ/Assignment/Assignment_data/assignment_idaccuracy/Aminer
 #add.R 是脚本名称 后面是输入的参数
--- add.R
+++ add.R
@@ -1,13 +1,22 @@
 
+#!/usr/bin/env Rscript
+
 #commandArgs函数可以从命令⾏读⼊参数
 args <- commandArgs(trailingOnly = TRUE) 
 
-#第一个参数
+#处理命令行参数
 datapath <- args[1]
 
-setwd(datapath)
-list <- list.files()
-data_list <- lapply(list, read.csv)
-df2 <- do.call(rbind, data_list)
+#定义add函数
+add <- function(datapath){
+  setwd(datapath)
+  list <- list.files()
+  data_list <- lapply(list, read.csv)
+  df2 <- do.call(rbind, data_list)
+  return(df2)
+}
 
+result <- add(datapath)
 head(df2)
+
+write.csv(result,file="/Users/mawenting/Documents/Bigdata_econ/Assignment")
